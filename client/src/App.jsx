b6baf0f2030678c1abc49de82748d73f0819796a
soham-andhale <soham.andhale3@gmail.com>
2026-07-06 22:41:41 +0530

    created forgot password page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Forgotpassword from "./pages/forgotpassword";
 import "./index.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<Forgotpassword />} />
         <Route path="/" element={<Navigate to="/signup" replace />} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/forgotpassword.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Mail, Lock, User, ArrowRight } from "lucide-react";
+
+function Forgotpassword(){
+    return(
+        <div></div>
+    )
+}
+
+export default Forgotpassword;
